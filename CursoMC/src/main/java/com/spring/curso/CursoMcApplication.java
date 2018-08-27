cc98a3134dd6bc76c3d130025ff1d5a06ab16504
DoglasProg <dogllas12@hotmail.com>
2018-08-27 02:32:18 -0300

    criação de Instanciação dos dados

--- CursoMC/src/main/java/com/spring/curso/CursoMcApplication.java
+++ CursoMC/src/main/java/com/spring/curso/CursoMcApplication.java
@@ -1,12 +1,35 @@
 package com.spring.curso;
 
+
+
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.spring.curso.domain.Categoria;
+import com.spring.curso.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRepository.save(cat1);
+		categoriaRepository.save(cat2);
+
 	}
 }
