64705f49be17da846fd7776a9fde6ff599088996
DoglasProg <dogllas12@hotmail.com>
2018-08-27 02:05:06 -0300

    Criação de Service e Repository para Categoria

---
+++ CursoMC/src/main/java/com/spring/curso/Services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.spring.curso.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.curso.domain.Categoria;
+import com.spring.curso.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	//a dependência vai ser automaticamente instanciada pelo Spring
+	
+	public Categoria buscar(Integer id) {
+		//Optional container para encapsular Categoria, solução para erro null
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+}
--- CursoMC/src/main/java/com/spring/curso/resources/CateegoriaResouce.java
+++ CursoMC/src/main/java/com/spring/curso/resources/CateegoriaResouce.java
@@ -3,26 +3,28 @@ package com.spring.curso.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.curso.Services.CategoriaService;
 import com.spring.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CateegoriaResouce {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		//anotação para indicar que recebe um argumento
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
