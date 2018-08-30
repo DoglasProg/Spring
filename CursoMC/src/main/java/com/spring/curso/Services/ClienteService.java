625d0639a5a86917294b7b124519b26340d14556
DoglasProg <dogllas12@hotmail.com>
2018-08-30 01:19:27 -0300

    Endpoint /clientes/{id}

---
+++ CursoMC/src/main/java/com/spring/curso/Services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.spring.curso.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.curso.Services.exception.ObjectNotFoundException;
+import com.spring.curso.domain.Cliente;
+import com.spring.curso.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ", Tipo:" + Cliente.class.getName()));
+		
+	}
+}
