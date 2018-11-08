9481af68453ad4b7254578cf524a25a244c7511c
DoglasProg <dogllas12@hotmail.com>
2018-11-07 23:37:29 -0300

    paginação com parametros opicionais na requisicao

--- CursoMC/src/main/java/com/spring/curso/Services/CategoriaService.java
+++ CursoMC/src/main/java/com/spring/curso/Services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.spring.curso.Services.exception.ObjectNotFoundException;
@@ -44,10 +47,14 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e){
 			throw new DataIntegrityViolationException("Não é possivel excluir uma categoria que possui produtos");
 		}
-		
 	}
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
