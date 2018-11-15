b636d33841cb3becdbca6e201dee52386795f7e9
DoglasProg <dogllas12@hotmail.com>
2018-11-15 11:14:52 -0300

    Validação de email não repetido em cliente

--- CursoMC/src/main/java/com/spring/curso/dto/ClienteDTO.java
+++ CursoMC/src/main/java/com/spring/curso/dto/ClienteDTO.java
@@ -7,8 +7,10 @@ import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.spring.curso.Services.validation.ClienteUpdate;
 import com.spring.curso.domain.Cliente;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
